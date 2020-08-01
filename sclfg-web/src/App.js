b60c35478d7bc971f2d0612b4e917fc8b5adbdbc
Griffin Johnson <cougargriff@gmail.com>
2020-07-31 17:02:33 -0700

    init

---
+++ sclfg-web/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./App.css";
+import logo from "./SCLFG_LOGO.png";
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <h1>
+          SCLFG TOOL
+        </h1>
+      </center>
+      <img src={logo} />
+    </div>
+  );
+}
+
+export default App;
